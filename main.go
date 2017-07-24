42a4a82c43dde765854c05edade21e6b38b3d3a0
Nihad Abbasov <nihad@42na.in>
2017-07-24 14:23:29 +0400

    initial commit

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CurrencyData struct {
+	Currencies map[string]Currency `json:"results"`
+}
+
+type Currency struct {
+	Name string `json:"currencyName"`
+}
+
+func loadCurrencies() CurrencyData {
+	var data CurrencyData
+
+	// List of all currencies from http://free.currencyconverterapi.com/api/v3/currencies
+	file, _ := ioutil.ReadFile("./currencies.json")
+	json.Unmarshal(file, &data)
+
+	return data
+}
+
+func getCurrencyNames(from string, to string) (string, string) {
+	data := loadCurrencies()
+	fromCurrencyName := data.Currencies[from].Name
+	toCurrencyName := data.Currencies[to].Name
+
+	return fromCurrencyName, toCurrencyName
+}
+
+func isValidCurrency(currencyId string) bool {
+	data := loadCurrencies()
+	currencyName := data.Currencies[currencyId].Name
+
+	return currencyName != ""
+}
+
+func handleError(message string) {
+	fmt.Println(message)
+	os.Exit(1)
+}
+
+func getRate(rateId string) float64 {
+	url := "http://free.currencyconverterapi.com/api/v3/convert?q=" + rateId + "&compact=ultra"
+
+	response, err := http.Get(url)
+	if err != nil {
+		handleError("Error getting data")
+	}
+	defer response.Body.Close()
+
+	var data map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		handleError("Error fetching data")
+	}
+
+	rate := data[rateId]
+	if rate == nil {
+		handleError("No results for currency rate " + rateId)
+	}
+
+	return rate.(float64)
+}
+
+func validArgs(args []string) (float64, string, string) {
+	if len(args) < 5 {
+		handleError("Insufficient arguments")
+	}
+
+	from, to := strings.ToUpper(args[2]), strings.ToUpper(args[4])
+	if !isValidCurrency(from) || !isValidCurrency(to) {
+		handleError("Invalid currency code")
+	}
+
+	amount, err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		handleError("Invalid value for conversion")
+	}
+
+	return amount, from, to
+}
+
+func main() {
+	amount, from, to := validArgs(os.Args)
+
+	result := amount * getRate(from+"_"+to)
+	fromCurrency, toCurrency := getCurrencyNames(from, to)
+
+	fmt.Println(amount, fromCurrency, "=", result, toCurrency)
+}
---
+++ main_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"testing"
+
+	"gopkg.in/jarcoal/httpmock.v1"
+)
+
+func TestValidArgs(t *testing.T) {
+	args := []string{"", "9000.3", "eur", "to", "usd"}
+	amount, from, to := validArgs(args)
+
+	if amount != 9000.3 {
+		t.Fatalf("Expected '9000.3' got '%v'", amount)
+	}
+
+	if from != "EUR" {
+		t.Fatalf("Expected 'EUR' got '%v'", from)
+	}
+
+	if to != "USD" {
+		t.Fatalf("Expected 'USD' got '%v'", to)
+	}
+}
+
+func TestLoadCurrencies(t *testing.T) {
+	data := loadCurrencies()
+
+	if data.Currencies == nil {
+		t.Fatalf("Expected 'Currencies' struct got nil")
+	}
+}
+
+func TestGetCurrencyNames(t *testing.T) {
+	name1, name2 := getCurrencyNames("EUR", "USD")
+
+	if name1 != "European Euro" {
+		t.Fatalf("Expected 'European Euro' got '%v'", name1)
+	}
+
+	if name2 != "United States dollar" {
+		t.Fatalf("Expected 'United States dollar' got '%v'", name2)
+	}
+}
+
+func TestIsValidCurrency(t *testing.T) {
+	valid := isValidCurrency("USD")
+	invalid := isValidCurrency("invalid")
+
+	if valid != true {
+		t.Fatalf("Expected 'true' got '%v'", valid)
+	}
+
+	if invalid != false {
+		t.Fatalf("Expected 'false' got '%v'", invalid)
+	}
+}
+
+func TestGetRate(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", "http://free.currencyconverterapi.com/api/v3/convert?q=EUR_USD&compact=ultra",
+		httpmock.NewStringResponder(200, `{"EUR_USD":700.007}`))
+
+	rate := getRate("EUR_USD")
+
+	if rate != 700.007 {
+		t.Fatalf("Expected '700.007' got '%v'", rate)
+	}
+}
